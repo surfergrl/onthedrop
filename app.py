924e03a6f2eb738c3c3796d27a8f129616964dfd
Kat <105393029+surfergrl@users.noreply.github.com>
2025-05-13 19:33:39 +0100

    Configure port binding for Heroku

--- app.py
+++ app.py
@@ -99,3 +99,7 @@ def delete(id):
 
 if __name__ == '__main__':
     app.run(debug=False)
+
+    if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
