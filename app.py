9c068c5413e1eedf0e1018c881e7e2acb5d8f582
Kat <105393029+surfergrl@users.noreply.github.com>
2025-05-14 20:15:05 +0100

    Add required commas to variables if block

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG")
-        SECRET_KEY=os.environ.get("SECRET_KEY")
+        debug=os.environ.get("DEBUG"),
+        secret_key=os.environ.get("SECRET_KEY"),
     )
 
 # Database initialization
