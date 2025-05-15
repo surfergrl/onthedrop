306485c6767646327bc01eb697a5baf7d39d4d81
Kat <105393029+surfergrl@users.noreply.github.com>
2025-05-15 11:16:51 +0100

    import env into app.py properly

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import sqlite3
-import os
+import os 
+import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG"),
-        secret_key=os.environ.get("SECRET_KEY"),
     )
 
 # Database initialization
